8795c30b4305e8b0b4b642145a9ae3c81ff0cfae
Joud Awad <joud.awad@metalabdesign.com>
2022-10-29 16:20:36 +0300

    adding the first part of the github actions

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,23 @@
 const core = require("@actions/core");
+// github package provides connection with github API/graphql
 const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  // 1) get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: false });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // 2) upload files
+  /**
+   * Typically we should use the AWS SDK to preform live connection to the AWS S3
+   * but in our case we are running the script on ubuntu-latest which comes with shipped AWS-CLI installed on it
+   * so we can run a command instead of using the AWS SDK which will do exactly the same
+   */
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom Javascript Action");
 }
 
